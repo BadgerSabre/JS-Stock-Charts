a62ca9f52ff0d1f3e8d547769e41d08704aaf626
Brandon Dega <brandondega@gmail.com>
2022-04-11 11:34:52 -0500

    added stock API

--- public/index.js
+++ public/index.js
@@ -1,8 +1,13 @@
 async function main() {
-
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+    const { GME, MSFT, DIS, BNTX } = result;
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    let res = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=ae4bf80cd848465bb15ae550fc2f5bbb')
+    let resJson = await res.json()
+    console.log(resJson)
 
 }
 

89aa5d1c49c93994f0adfbc3522947c5041de0a6
Brandon Dega <brandondega@gmail.com>
2022-04-11 12:03:30 -0500

    added chart

--- public/index.js
+++ public/index.js
@@ -2,13 +2,13 @@ async function main() {
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+    
+    let result = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=ae4bf80cd848465bb15ae550fc2f5bbb')
+    let resJson = await result.json()
+    console.log(resJson)
+    
     const { GME, MSFT, DIS, BNTX } = result;
     const stocks = [GME, MSFT, DIS, BNTX];
-
-    let res = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=ae4bf80cd848465bb15ae550fc2f5bbb')
-    let resJson = await res.json()
-    console.log(resJson)
-
 }
 
 main()

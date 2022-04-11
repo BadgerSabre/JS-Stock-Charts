d1adde94e6b44b9cdad37d30dcce8812f1e4fb18
Brandon Dega <brandondega@gmail.com>
2022-04-11 17:41:03 -0500

    added bar and pie charts

--- public/index.js
+++ public/index.js
@@ -7,10 +7,66 @@ async function main() {
     let resJson = await result.json()
     console.log(resJson)
     
-    const { GME, MSFT, DIS, BNTX } = result;
+    const { GME, MSFT, DIS, BNTX } = mockData;
     const stocks = [GME, MSFT, DIS, BNTX];
 
+    stocks.forEach( stock => stock.values.reverse())
+    //Time Chart
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map( stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor:  getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+        }
+    });
+    //Highest Price Chart
+    new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks.map(value => value.meta.symbol),
+            datasets: stocks.map( stock => ({
+                label: stock.meta.symbol,
+                data: stocks[0].values.map(value => parseFloat(value.high)),
+                backgroundColor:  getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+        }
+    });
+    //Average Stock Price Chart
+    new Chart(averagePriceChartCanvas.getContext('2d'), {
+        type: 'pie',
+        data: {
+            labels: stocks.map(value => value.meta.symbol),
+            datasets: stocks.map( stock => ({
+                label: stock.meta.symbol,
+                data: stocks[0].values.map(value => parseFloat(value.high)),
+                backgroundColor:  getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+        }
+    });
+
     console.log(stocks[0].values)
+}
+
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
 }
 
 main()
